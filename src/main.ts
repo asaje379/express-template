f4fc57497b3b084415d93a4dbf33881183d77104
Salem AFFA <salemaffa@gmail.com>
2023-09-24 09:57:45 +0000

    Update 3 files

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import places from './places.json';
+import express, { Response } from "express";
+import cors from "cors";
+import places from "./places.json";
 
 console.log(places);
 
 const app = express();
 app.use(cors());
 
+app.get("/", (_, res: Response) => {
+  res.status(200).send("Hello world");
+});
+
 app.listen(3000, () => {
-  console.log('Server is running...');
+  console.log("Server is running...");
 });
